fb40235da45e5e140c4f4120c5afc4b673b2a1a8
Bo Anderson <mail@boanderson.me>
2023-01-27 03:31:12 +0000

    actions/sync/triage-config.rb: migrate from deprecated set-output

--- .github/actions/sync/triage-config.rb
+++ .github/actions/sync/triage-config.rb
@@ -35,7 +35,7 @@ raise err unless status.success?
 target_dir_changed = !out.chomp.empty?
 
 unless target_dir_changed
-  puts 'No changed detected.'
+  puts 'No changes detected.'
   exit
 end
 
@@ -52,4 +52,6 @@ modified_paths.each do |modified_path|
 end
 puts
 
-puts '::set-output name=pull_request::true'
+File.open(ENV.fetch('GITHUB_OUTPUT', 'a')) do |f|
+  f.puts 'pull_request=true'
+end
