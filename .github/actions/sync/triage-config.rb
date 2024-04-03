36149bee7d9fccd066b5f11422124e86e6e6a828
Bevan Kay <email@bevankay.me>
2024-04-03 18:43:26 +1100

    triage-config: keep old path for now to remove
    I think we need to retain the old `triage-issue` path to ensure it is removed from the other repos. Unless we manually remove it.
    
    CC: @MikeMcQuaid

--- .github/actions/sync/triage-config.rb
+++ .github/actions/sync/triage-config.rb
@@ -16,6 +16,7 @@ puts 'Detecting changes…'
 [
   '.github/workflows/lock-threads.yml',
   '.github/workflows/stale-issues.yml',
+  '.github/workflows/triage-issues.yml',
 ].each do |glob|
   src_paths = Pathname.glob(glob)
   dst_paths = Pathname.glob(target_dir.join(glob))
