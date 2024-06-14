d5ae259a98343504c36374a2f0c19317e75ac938
Mike McQuaid <mike@mikemcquaid.com>
2024-06-14 15:42:21 +0100

    actions/sync/shared-config: fix relative symlinks.
    The previous implementation was not working as expected.

--- .github/actions/sync/shared-config.rb
+++ .github/actions/sync/shared-config.rb
@@ -134,7 +134,9 @@ puts "Detecting changes…"
       if [ruby_version, "Gemfile", "Gemfile.lock"].include?(docs_path_basename) &&
          (target_path/docs_path_basename).exist?
         FileUtils.rm target_docs_path
-        File.symlink((target_path/docs_path_basename).relative_path_from(target_docs_path.dirname), target_docs_path)
+        Dir.chdir target_path do
+          FileUtils.ln_s "../#{docs_path_basename}", "."
+        end
       else
         FileUtils.cp docs_path, target_docs_path
       end
